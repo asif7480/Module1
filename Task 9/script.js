bf299a952b838d65251d372f90bbac5e523dd9af
asif7480 <a.a.khan7480@gmail.com>
2022-08-07 01:38:04 +0500

    Module 1

---
+++ Task 9/script.js
@@ -0,0 +1,25 @@
+const validate = () => {
+    let text = document.querySelector("#text").value;
+    if (text[0] === text[0].toUpperCase()) {
+      return true;
+    } else {
+      alert("Enter First letter capital of full name.");
+      return false;
+    }
+  };
+  
+  const validatePassword = () => {
+    let password = document.querySelector("#password").value;
+    if (
+      password.length >= 8 &&
+      password.match(/[0-9]/g) &&
+      password.match(/[a-z]/g) &&
+      password.match(/[A-Z]/g) &&
+      password.match(/[^a-zA-Z\d]/g)
+    ) {
+      return true;
+    } else {
+      alert("Password Should Contain Atleast 1 Number, 1 Upppercase Letter, 1 Lowercase Letter, 1 Special Character and Length of 8 Characters or more.");
+      return false;
+    }
+  };
